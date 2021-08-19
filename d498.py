a6c6344a580b3c00ceaa073a14077497d1711544
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2021-08-20 00:24:01 +0800

    Create d498.py

---
+++ d498.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        try:
+            n = int(input())
+        except:
+            break
+
+        for i in range(n):
+            print("I don't say swear words!")
+
+main()
